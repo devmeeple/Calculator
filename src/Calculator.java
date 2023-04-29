0b4231707e8fe2105cc7bfcad1071d3786c8a95e
devmeeple <devmeeple@gmail.com>
2023-04-29 10:12:20 +0900

    반복문을 통한 입력받기, 계산기 기본기능 구현완료

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,40 +1,52 @@
-//https://www.youtube.com/watch?v=DPTCIw0mHWI&list=PLqaSEyuwXkSopmCFiv0v7seTIRN5rrEtP&index=20
-//사용자에게 2개의 숫자 값과 사칙연산 기호(+, -, *, /) 중 하나를 입력받는다.
-//사용자가 입력한 사칙연산 기호에 따라 사칙연산을 계산한 후 결과를 출력한다.
-//예를 들어 사용자는 사용자는 값 2와 4 입력하고 곱셈(*) 기호를 입력하면 2 * 4의 결과인 8을 출력해야 한다.
+//https://www.youtube.com/watch?v=HvAByhhKu-E&list=PLqaSEyuwXkSopmCFiv0v7seTIRN5rrEtP&index=22
+//한 번에 2개의 숫자 값만 사칙연산을 하는 것이 아니라 여러 개의 사칙연산이 가능해야 한다.
+//앞 단계에서 계산한 결과 값은 이후 연산의 첫번째 값으로 사용해야 한다.
+//    예를들어 4 + 2 * 3의 순서로 값을 입력하면 먼저 4+2가 계산되고 결과 값 6과 3의 곱셈을 통해 18의 계산 결과가 나와야 한다.
+//사칙연산 기호 대신 "quit"이라는 문자열을 입력하면 최종 계산 결과를 출력하고 프로그램을 종료한다.
+
 import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        int num1, num2;
         Scanner sc = new Scanner(System.in);
         System.out.println("<사칙연산 프로그램> 입니다.");
-        System.out.println("----------------------");
+        System.out.print("숫자를 입력하세요 > ");
+        int num1 = sc.nextInt();
+        int result = num1;
+        while (true) {
+            System.out.println("----------------------");
+            System.out.println("명령을 입력해주세요. ");
+            System.out.println("사칙연산 : (+, -, *, /)");
+            System.out.println("프로그램종료 : quit");
+            System.out.print("> ");
 
-        System.out.print("숫자를 입력해주세요 > ");
-        num1 = sc.nextInt();
+            String symbol = sc.next();
 
-        System.out.print("사칙연산 기호(+, -, *, /)를 입력해주세요 > ");
-        String symbol = sc.next();
+            // 무한루프 방지 종료 구문
+            if("quit".equals(symbol)) {
+                System.out.println("연산결과입니다.");
+                System.out.println(result);
+                break;
+            }
 
-        System.out.print("숫자를 입력해주세요 > ");
-        num2 = sc.nextInt();
+            System.out.print("숫자를 입력해주세요 > ");
+            int num2 = sc.nextInt();
 
+            if("+".equals(symbol)) {
+                result = result + num2;
+            } else if ("-".equals(symbol)) {
+                result = result - num2;
+            } else if ("*".equals(symbol)) {
+                result = result * num2;
+            } else if("/".equals(symbol)) {
+                result = result / num2;
+            } else {
+                System.out.println("사칙연산기호를 입력하지 않았습니다. 다시 실행해주세요.");
+            }
 
-        System.out.println("입력하신 숫자와 기호는 " + num1 + "," + symbol + "," + num2 +" 입니다");
-        System.out.println("-----연산결과입니다.-----");
-
-        if("+".equals(symbol)) {
-            System.out.println("더하기(+) : " + (num1 + num2));
-        } else if ("-".equals(symbol)) {
-            System.out.println("빼기(-) : " + (num1 - num2));
-        } else if ("*".equals(symbol)) {
-            System.out.println("곱하기(*) : " + (num1 * num2));
-        } else if("/".equals(symbol)) {
-            System.out.println("나누기(/) : " + (num1 / num2));
-        } else {
-            System.out.println("사칙연산기호를 입력하지 않았습니다. 다시 실행해주세요.");
         }
         System.out.println("----------------------");
+
 
     }
 }
