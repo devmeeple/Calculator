1ec4e116efd0ac9c604929d46dd88eced2f26826
devmeeple <devmeeple@gmail.com>
2023-05-01 11:25:11 +0900

    메소드를 분리해 계산기 기능 구현

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,48 +1,69 @@
-//https://www.youtube.com/watch?v=HvAByhhKu-E&list=PLqaSEyuwXkSopmCFiv0v7seTIRN5rrEtP&index=22
-//한 번에 2개의 숫자 값만 사칙연산을 하는 것이 아니라 여러 개의 사칙연산이 가능해야 한다.
-//앞 단계에서 계산한 결과 값은 이후 연산의 첫번째 값으로 사용해야 한다.
-//    예를들어 4 + 2 * 3의 순서로 값을 입력하면 먼저 4+2가 계산되고 결과 값 6과 3의 곱셈을 통해 18의 계산 결과가 나와야 한다.
-//사칙연산 기호 대신 "quit"이라는 문자열을 입력하면 최종 계산 결과를 출력하고 프로그램을 종료한다.
+//https://www.youtube.com/watch?v=vLJtpj5v98g&list=PLqaSEyuwXkSopmCFiv0v7seTIRN5rrEtP&index=22
+//사용자의 값을 입력하는 구현(예로 input 메소드), 입력 값에 따라 사칙연산 구현(예로 main 메소드), 사칙연산 결과 값을
+//출력하는 구현(예로 output 메소드)을 서로 다른 메소드로 나누어 구현한다.
+//사용자의 값을 입력하는 구현은 첫 번째 숫자 값, 사칙연산 기호 및 quit, 두 번째 숫자 값 입력으로 나뉜다.
 
 import java.util.Scanner;
 
 public class Calculator {
+    static int getFirstValue(Scanner sc) {
+        System.out.print("숫자를 입력하세요 > ");
+        int first = sc.nextInt();
+        return first;
+    }
+    static int getSecondValue(Scanner sc) {
+        System.out.print("숫자를 입력해주세요 > ");
+        int second = sc.nextInt();
+        return second;
+    }
+
+    static String getSymbol(Scanner sc) {
+        System.out.println("사칙연산 : (+, -, *, /)");
+        System.out.println("프로그램종료 : quit");
+        System.out.print("> ");
+        String symbol = sc.next();
+        return symbol;
+    }
+
+    static int calculate(int first, String symbol, int second) {
+        int result = 0;
+        if("+".equals(symbol)) {
+            result = first + second;
+            System.out.println("덧셈 : " + result);
+            
+        } else if ("-".equals(symbol)) {
+            result = first - second;
+            System.out.println("뺄셈 : " + result);
+        } else if ("*".equals(symbol)) {
+            result = first * second;
+            System.out.println("곱셈 : " + result);
+        } else if("/".equals(symbol)) {
+            result = first / second;
+            System.out.println("나눗셈 : " + result);
+        } else {
+            System.out.println("사칙연산기호를 입력하지 않았습니다. 다시 실행해주세요.");
+        }
+        return result;
+    }
+
+    static void print(int result) {
+        System.out.println("결과 : " +result);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("<사칙연산 프로그램> 입니다.");
-        System.out.print("숫자를 입력하세요 > ");
-        int num1 = sc.nextInt();
-        int result = num1;
+        int first = getFirstValue(sc);
+        int result = first;
         while (true) {
-            System.out.println("----------------------");
-            System.out.println("명령을 입력해주세요. ");
-            System.out.println("사칙연산 : (+, -, *, /)");
-            System.out.println("프로그램종료 : quit");
-            System.out.print("> ");
-
-            String symbol = sc.next();
-
+            String symbol = getSymbol(sc);
             // 무한루프 방지 종료 구문
             if("quit".equals(symbol)) {
-                System.out.println("연산결과입니다.");
-                System.out.println(result);
+                print(result);
                 break;
             }
+            int second = getSecondValue(sc);
 
-            System.out.print("숫자를 입력해주세요 > ");
-            int num2 = sc.nextInt();
-
-            if("+".equals(symbol)) {
-                result = result + num2;
-            } else if ("-".equals(symbol)) {
-                result = result - num2;
-            } else if ("*".equals(symbol)) {
-                result = result * num2;
-            } else if("/".equals(symbol)) {
-                result = result / num2;
-            } else {
-                System.out.println("사칙연산기호를 입력하지 않았습니다. 다시 실행해주세요.");
-            }
+            result = calculate(result, symbol, second);
 
         }
         System.out.println("----------------------");
